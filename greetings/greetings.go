9f7e0853da9a51d9ee1093934badb141cfb798c1
Sid Raj <siddharth.rajesh13@gmail.com>
2020-09-27 08:23:10 +1000

    greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -20,6 +20,26 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+
+	// map associated names with message
+	messages := make(map[string]string)
+
+	// loop through slice of names, call Hello function to get greeting for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// associate greeting with name
+		messages[name] = message
+	}
+	return messages, nil
+
+}
+
 // set initial values for variables used in the function
 func init() {
 	rand.Seed(time.Now().UnixNano())
